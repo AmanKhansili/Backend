99a669232c47eba7492739408aace78de57e20fa
Aman Khansili <amankhansilee@gmail.com>
2024-11-29 00:02:47 +0530

    add user and video model learn about jwt and bcrypt

--- src/app.js
+++ src/app.js
@@ -4,22 +4,19 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-//whenever we are using middleware we are using app.use.
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
-//get data from form
-app.use(express.json({ limit: "16kb" }));
+// Middleware order matters!
 
-//get data from url
+// CORS setup (allow frontend to access backend)
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+// Parse incoming JSON and form data
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
 
-//public folder
+// Serve static files (like images, CSS, JS)
 app.use(express.static("public"));
 
-//allow us to do some secure CRUD operations
+// Parse cookies
 app.use(cookieParser());
+
 export default app;
